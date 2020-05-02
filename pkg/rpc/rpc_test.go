30be936ab092669983931895174742b2cad9a445
gunslinger <gunslinger-ovo@qq.com>
2020-05-03 00:53:57 +0800

    Update rpc_test.go

--- pkg/rpc/rpc_test.go
+++ pkg/rpc/rpc_test.go
@@ -20,4 +20,7 @@ func TestAll(t *testing.T) {
 	if GetError(err).Code != codes.Internal {
 		t.Errorf("Error code: v = %d, want = 13", GetError(err).Code)
 	}
+	if GetError(nil) != nil {
+		t.Errorf("GetError(): v = %v, want = nil", GetError(err))
+	}
 }
