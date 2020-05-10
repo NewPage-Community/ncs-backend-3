87ce4d22edf259f20b77205ad5c8b620439cdbf1
Gunslinger <gunslinger-ovo@qq.com>
2020-05-11 06:46:42 +0800

    Update push images shell

---
+++ build/changed-pkgs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+files=$(git diff HEAD^ HEAD --name-only --diff-filter=ACM | grep -E -i ".go")
+
+pkgs=""
+for file in ${files}; do
+    pkg="${file%/*}"
+    if [ $? -eq 0 ]; then
+        if [[ "${pkgs}" = "" ]]; then
+            pkgs="${pkg}"
+        else
+            pkgs="${pkgs}\n${pkg}"
+        fi
+    fi
+done
+echo -e "${pkgs}" >/tmp/.pkgs
+pkgs=$(sort /tmp/.pkgs | uniq)
+echo -e "${pkgs}"
--- build/push-images.sh
+++ build/push-images.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
-pkgs=$(build/get-pkgs.sh ${1})
-exitCode=$?
-if [[ ${exitCode} -ne 0 ]]; then
-    echo "build/get-pkgs.sh fail"
-    exit ${exitCode}
-fi
-if [[ "${pkgs}" = "" ]]; then
-    echo "No changed packages"
-    exit 0
-fi
-
-echo -e "Packages for pushing:\n${pkgs}\n"
-
-paths=""
-for pkg in ${pkgs}; do
-    if [[ "${paths}" != "" ]]; then
-        paths="${paths} union allpaths(//app/..., //${pkg}:all)"
-    else
-        paths="allpaths(//app/..., //${pkg}:all)"
+function push_changed() {
+    pkgs=$(build/changed-pkgs.sh)
+    exitCode=$?
+    if [[ ${exitCode} -ne 0 ]]; then
+        echo "build/get-pkgs.sh fail"
+        exit ${exitCode}
     fi
-done
+    if [[ "${pkgs}" = "" ]]; then
+        echo "No changed packages"
+        exit 0
+    fi
 
-echo "Pushing image to docker..."
-query=$(bazel query "${paths}")
-bazel run --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64 -k $(echo -e "${query}" | grep push)
+    echo -e "Packages for pushing:\n${pkgs}\n"
+
+    paths=""
+    for pkg in ${pkgs}; do
+        if [[ "${paths}" != "" ]]; then
+            paths="${paths} union allpaths(//app/..., //${pkg}:all)"
+        else
+            paths="allpaths(//app/..., //${pkg}:all)"
+        fi
+    done
+
+    echo "Pushing image to docker..."
+    query=$(bazel query "${paths}")
+    bazel run --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64 -k $(echo -e "${query}" | grep push)
+}
+
+function push_all() {
+    echo "Pushing all image to docker..."
+    query=$(bazel query //app/...)
+    bazel run --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64 -k $(echo -e "${query}" | grep push)
+}
+
+if [[ "${1}" = "changed" ]]; then
+    push_changed
+else
+    push_all
+fi
