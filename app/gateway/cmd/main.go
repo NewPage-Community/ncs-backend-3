e6275ef3714f9576c5d7a401be24666cc3423334
Gunslinger <gunslinger-ovo@qq.com>
2020-06-11 16:12:57 +0800

    Fix gateway closing #2

--- app/gateway/cmd/main.go
+++ app/gateway/cmd/main.go
@@ -2,27 +2,42 @@ package main
 
 import (
 	"backend/app/gateway/service"
+	"backend/pkg/cmd"
+	"backend/pkg/log"
 	"backend/pkg/rpc"
-	"flag"
+	"context"
 	"net/http"
-
-	"github.com/golang/glog"
+	"time"
 )
 
-func run() error {
+func main() {
+	log.Init(&log.Config{Debug: true})
 	gateways := rpc.NewGateway()
 	service.RegService(gateways)
 	defer gateways.Close()
 
-	// Start HTTP server (and proxy calls to gRPC server endpoint)
-	return http.ListenAndServe(":8081", gateways.HTTPHandler())
-}
-
-func main() {
-	flag.Parse()
-	defer glog.Flush()
-
-	if err := run(); err != nil {
-		glog.Fatal(err)
+	srv := &http.Server{
+		Addr:           ":8081",
+		Handler:        gateways.HTTPHandler(),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Error(err)
+		}
+	}()
+
+	log.Info("Gateway started!")
+
+	cmd.Run("Gateway", func() {
+		ctx, _ := context.WithTimeout(context.Background(), time.Second*15)
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			log.Error(err)
+		}
+	})
 }
