da1e003d4323ad5baa8ecad8b587213d8f851f0c
Gunslinger <gunslinger-ovo@qq.com>
2020-06-23 21:55:21 +0800

    Update gateway
    Add pass reward service to gateway

--- app/gateway/service/pass.go
+++ app/gateway/service/pass.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	rewardGW "backend/app/service/pass/reward/api/grpc"
 	userGW "backend/app/service/pass/user/api/grpc"
 	"backend/pkg/rpc"
 )
@@ -9,5 +10,9 @@ func regPassService(gws *rpc.Gateways) {
 	gws.AddGateway(
 		userGW.RegisterUserHandlerFromEndpoint,
 		userGW.ServiceAddr,
+	)
+	gws.AddGateway(
+		rewardGW.RegisterRewardHandlerFromEndpoint,
+		rewardGW.ServiceAddr,
 	)
 }
