10e7b5b417c26c07fb88c98702ac48a37b361bca
Gunslinger <gunslinger-ovo@qq.com>
2020-08-17 00:13:26 +0800

    Update tracer

--- pkg/tracer/jaeger.go
+++ pkg/tracer/jaeger.go
@@ -3,13 +3,11 @@ package tracer
 import (
 	"backend/pkg/log"
 	"github.com/opentracing/opentracing-go"
-	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 	jaegerlog "github.com/uber/jaeger-client-go/log"
 	"github.com/uber/jaeger-client-go/zipkin"
 	"github.com/uber/jaeger-lib/metrics"
 	"io"
-	"time"
 )
 
 var tracerCloser io.Closer
@@ -19,16 +17,14 @@ func Init(serviceName string) {
 	cfg := jaegercfg.Configuration{
 		ServiceName: serviceName,
 		Reporter: &jaegercfg.ReporterConfig{
-			LogSpans:            false,
-			BufferFlushInterval: time.Second,
-			CollectorEndpoint:   "http://jaeger-collector.istio-system:14268/api/traces",
+			CollectorEndpoint: "http://jaeger-collector.istio-system:14268/api/traces",
 		},
 	}
 
 	// Example logger and metrics factory. Use github.com/uber/jaeger-client-go/log
 	// and github.com/uber/jaeger-lib/metrics respectively to bind to real logging and metrics
 	// frameworks.
-	jLogger := jaegerlog.StdLogger
+	jLogger := jaegerlog.NullLogger
 	jMetricsFactory := metrics.NullFactory
 
 	// Zipkin shares span ID between client and server spans; it must be enabled via the following option.
@@ -42,7 +38,6 @@ func Init(serviceName string) {
 		jaegercfg.Injector(opentracing.HTTPHeaders, zipkinPropagator),
 		jaegercfg.Extractor(opentracing.HTTPHeaders, zipkinPropagator),
 		jaegercfg.ZipkinSharedRPCSpan(true),
-		jaegercfg.Sampler(jaeger.NewConstSampler(true)),
 	)
 
 	if err != nil {
