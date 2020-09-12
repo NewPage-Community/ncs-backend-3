745cfe53eb0167cf38747fd561ab1e877399e984
Gunslinger <gunslinger-ovo@qq.com>
2020-09-12 20:58:53 +0800

    Update pass rewards configure

--- app/service/pass/reward/conf/conf.go
+++ app/service/pass/reward/conf/conf.go
@@ -1,23 +1,28 @@
 package conf
 
 import (
-	reward "backend/app/service/pass/reward/model"
 	"backend/pkg/conf"
 	"backend/pkg/database/mysql"
 	"backend/pkg/log"
 )
 
 type Config struct {
-	Log    *log.Config
-	Mysql  *mysql.Config
-	Reward *reward.Reward
+	Log        *log.Config
+	Mysql      *mysql.Config
+	RewardConf *RewardConf
+}
+
+type RewardConf struct {
+	Season int32
+	Free   string
+	Adv    string
 }
 
 func Init() (c *Config) {
 	c = &Config{
 		&log.Config{},
 		&mysql.Config{},
-		&reward.Reward{},
+		&RewardConf{},
 	}
 	conf.Load(c)
 	return
--- app/service/pass/reward/service/service.go
+++ app/service/pass/reward/service/service.go
@@ -3,16 +3,28 @@ package service
 import (
 	"backend/app/service/pass/reward/conf"
 	"backend/app/service/pass/reward/model"
+	"backend/pkg/json"
+	"backend/pkg/log"
 )
 
 type Service struct {
 	reward *model.Reward
 }
 
-func Init(config *conf.Config) *Service {
-	return &Service{
-		reward: config.Reward,
+func Init(config *conf.Config) (s *Service) {
+	s = &Service{
+		reward: &model.Reward{
+			Season: config.RewardConf.Season,
+		},
 	}
+	//json
+	if err := json.Unmarshal([]byte(config.RewardConf.Free), &s.reward.FreeRewards); err != nil {
+		log.Error(err)
+	}
+	if err := json.Unmarshal([]byte(config.RewardConf.Adv), &s.reward.AdvRewards); err != nil {
+		log.Error(err)
+	}
+	return
 }
 
 func (s *Service) Healthy() bool {
