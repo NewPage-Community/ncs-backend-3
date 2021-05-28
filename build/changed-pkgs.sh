ea3da92996c5c4a19c5234f55a4d32767c3970fe
Gunslinger <gunslinger-ovo@qq.com>
2021-05-28 22:37:44 +0800

    Fix check changed packages in ci

--- build/changed-pkgs.sh
+++ build/changed-pkgs.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-files=$(git diff HEAD^ HEAD --name-only --diff-filter=ACM | grep -E -i "\.(go|bazel)$")
+# Get last build success commit
+url="${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines?status=success&private_token=${CI_PRIVATE_TOKEN}"
+json=$(curl -s ${url})
+if [[ "$json" != "[]" && "$json" != "" ]]; then
+    last_commit=$(echo ${json} | python -c "import json; import sys; obj=json.load(sys.stdin); print obj[0]['sha'].encode('utf-8')")
+else
+    last_commit="HEAD^"
+fi
+
+# Get diff
+files=$(git diff ${last_commit} HEAD --name-only --diff-filter=ACM | grep -E -i "\.(go|bazel)$")
 
 pkgs=""
 for file in ${files}; do
