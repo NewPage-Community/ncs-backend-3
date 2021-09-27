eb6da2d3de2dd77964a170879323f3627634c50f
Gunslinger <gunslinger-ovo@qq.com>
2021-09-27 15:45:25 +0800

    Update build script [skip ci]

--- build/changed-pkgs.sh
+++ build/changed-pkgs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get last build success commit
-url="${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=master&private_token=${CI_PRIVATE_TOKEN}"
+url="https://git.new-page.xyz/api/v4/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=master&private_token=${CI_PRIVATE_TOKEN}"
 json=$(curl -s ${url})
 if [[ "$json" != "[]" && "$json" != "" ]]; then
     last_commit=$(echo ${json} | python2.7 -c "import json; import sys; obj=json.load(sys.stdin); print obj[0]['sha'].encode('utf-8')")
