fff1f0234a583fe8ce60cd79a30996f9bc844a99
Gunslinger <gunslinger-ovo@qq.com>
2022-04-06 17:21:51 +0800

    Update changed-pkgs.sh

--- build/changed-pkgs.sh
+++ build/changed-pkgs.sh
@@ -3,7 +3,7 @@
 # Get last build success commit
 url="https://git.new-page.xyz/api/v4/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=master&private_token=${CI_PRIVATE_TOKEN}"
 json=$(curl -s ${url})
-if [[ $json -ne "[]" && $json -ne "" ]]; then
+if [[ $json != "[]" && $json != "" ]]; then
     last_commit=$(echo ${json} | python2.7 -c "import json; import sys; obj=json.load(sys.stdin); print obj[0]['sha'].encode('utf-8')")
 else
     last_commit="HEAD^"
